432cef566d86940ca29934257c046ad7fd05d8c6
Henry Han <potatoeater0127@gmail.com>
2020-06-12 18:27:12 +0800

    (feat) set up view template and clean task

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const $ = require("gulp-load-plugins")();
 const browserSync = require("browser-sync").create();
+const args = require("minimist")(process.argv.slice(2));
 
 function sass() {
   return gulp
@@ -20,5 +21,20 @@ function browser() {
   });
 }
 
+function html() {
+  return gulp
+    .src("./src/views/**/*.html")
+    .pipe($.plumber())
+    .pipe($.frontMatter())
+    .pipe($.layout((file) => file.frontMatter))
+    .pipe(gulp.dest("./dist/views"));
+}
+
+function clean() {
+  return gulp.src("./dist", { read: false, allowEmpty: true }).pipe($.clean());
+}
+
 exports.sass = sass;
 exports.browser = browser;
+exports.html = html;
+exports.clean = clean;
